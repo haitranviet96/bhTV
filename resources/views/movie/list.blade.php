af64a6de8ed670a23e21541f3475c97da80e0cac
Buithienbao <thienbao_bui@yahoo.com>
2018-05-08 15:21:58 +0700

    test

--- resources/views/movie/list.blade.php
+++ resources/views/movie/list.blade.php
@@ -45,7 +45,7 @@
         @if(isset($film['trailer_path']))
         <a href="{{$film['trailer_path']}}" class="btn btn-default">Watch Trailer</a>
         @endif
-        <a title="Click to add to watchlist" id="addToWishList" class="btn btn-default">
+        <a title="Click to add to wishlist" id="addToWishList" class="btn btn-default">
             <input type="hidden" id="film_id" value="{{$film['id']}}">
             Add to Wishlist
         </a>
